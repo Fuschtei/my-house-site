1bde3024b0bd5af55a60f782d29d37e44b3f4a83
Fushtei Sviatoslav <fuschtei@gmail.com>
2021-11-08 00:33:12 +0200

    add logic for burger menu

--- scripts/header.js
+++ scripts/header.js
@@ -1,3 +1,22 @@
 const burger = document.querySelector('.burger-icon');
-const burgerFunction = () => {alert('hello')};
+const burgerFunction = () => {
+    document.body.classList.add("lock");
+    const navOpenMenu = document.querySelector('.nav-open');
+    navOpenMenu.innerHTML +=
+            `    
+            <a href="#">About</a>
+            <a href="#">Gallery</a>
+            <a href="#">Accommodation</a>
+            <a href="#">How to get there</a>
+            <div>
+                <div>Yosemite Mariposa</div>
+                <div>County, Ca, USA</div>
+            </div>
+            <div>
+                <div>+1 (234) 567 89 00</div>
+                <div>+1 (234) 567 89 01</div>
+            </div>
+            `
+};
 burger.addEventListener("click", burgerFunction);
+    
