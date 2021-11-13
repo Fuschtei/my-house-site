c46fff330501eb140e38f50f47eed9a86c0c17c7
Fushtei Sviatoslav <fuschtei@gmail.com>
2021-11-13 16:46:00 +0200

    finished works with footer

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,2 +1,25 @@
 const footer = document.querySelector("#footer");
-footer.innerHTML += ``;
+footer.innerHTML += `
+<div class="container" id="footer-content">
+            <a class="image-logo-a" href="index.html">
+                <img class="image-logo footer-logo-img" src="images/logo.JPG" alt="logo-image">
+            </a>
+            <a href="#" class="font-color-282520 roboto-font" id="footer-about">About</a>
+            <a href="#" class="space font-color-282520 roboto-font" id="footer-rental-policies">Rental policies</a>
+            <a href="#" class="font-color-282520 roboto-font" id="footer-photos">Photos</a>
+            <a href="#" class="space font-color-282520 roboto-font" id="footer-how-to-get-there">How to get there</a>
+            <a href="#" class="font-color-282520 roboto-font" id="footer-accommodation">Accommodation</a>
+            <div class="follow-us allerta-font font-color-282520">Follow Us</div>
+            <div class="social-networks">
+                <a href="https://www.facebook.com/"><img src="images/social-networks/facebook.svg"
+                        alt="facebook-icon"></a>
+                <a href="https://twitter.com/"><img src="images/social-networks/twitter.svg" alt="twitter-icon"></a>
+                <a href="https://www.instagram.com/"><img src="images/social-networks/instagram.svg"
+                        alt="instagram-icon"></a>
+                <a href="https://www.linkedin.com/"><img src="images/social-networks/linkedin.svg"
+                        alt="linkedin-icon"></a>
+            </div>
+            <div id="author" class="space font-color-282520 roboto-font">© Created by: Sviatoslav Fushtei</div>
+            <div id="rights" class="space font-color-282520 roboto-font">All rights Reserved</div>
+        </div>
+        `;
