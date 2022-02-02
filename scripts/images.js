c9dc0ae7a51e8b01a3b4e9adbf85dbfc34a1d291
Fushtei Sviatoslav <fuschtei@gmail.com>
2022-02-02 18:00:07 +0200

    fixed

--- scripts/images.js
+++ scripts/images.js
@@ -21,14 +21,13 @@ card.forEach(function (i) {
         darkBackground.classList.remove("display-none");
 
         let target = event.target.id;
-        console.log(target);
+        let height = 130;
 
         if (target == `houses-on-hill-img`) {
             modalPhotoWindow.innerHTML = `<img
             style="
-            height:100%;
-            width: 100%;
-            overflow-x: hidden;
+            width: ${height}%;
+            margin: auto;
             "
             src="https://res2.weblium.site/res/5ce40621b84b1a002410eb9e/5ce425c34045b70023557d9f_optimized"
             alt="houses on hill">`
@@ -36,9 +35,8 @@ card.forEach(function (i) {
         else if (target == `street-in-village-img`) {
             modalPhotoWindow.innerHTML = `<img
             style="
-            height:100%;
-            width: 100%;
-            overflow-x: hidden;
+            width: ${height}%;
+            margin: auto;
             "
             src="https://res2.weblium.site/res/5ce40621b84b1a002410eb9e/5ce4246b72358b0023fc4948_optimized"
             alt="street in village">`
@@ -46,9 +44,8 @@ card.forEach(function (i) {
         else if (target == `big-house-img`) {
             modalPhotoWindow.innerHTML = `<img
             style="
-            height:100%;
-            width: 100%;
-            overflow-x: hidden;
+            width: ${height}%;
+            margin: auto;
             "
             src="https://res2.weblium.site/res/5ce40621b84b1a002410eb9e/5ce4259ec9f1c1002318991b_optimized"
             alt="big house">`
@@ -56,9 +53,8 @@ card.forEach(function (i) {
         else if (target == `lake-img`) {
             modalPhotoWindow.innerHTML = `<img
             style="
-            height:100%;
-            width: 100%;
-            overflow-x: hidden;
+            width: ${height}%;
+            margin: auto;
             "
             src="https://res2.weblium.site/res/5ce40621b84b1a002410eb9e/5ce423eadc5e3800248ca0ab_optimized"
             alt="lake">`
@@ -66,9 +62,8 @@ card.forEach(function (i) {
         else if (target == `mountains-img`) {
             modalPhotoWindow.innerHTML = `<img
             style="
-            height:100%;
-            width: 100%;
-            overflow-x: hidden;
+            width: ${height}%;
+            margin: auto;
             "
             src="https://res2.weblium.site/res/5ce40621b84b1a002410eb9e/5ce4221972358b0023fc470e_optimized"
             alt="mountains">`
@@ -76,9 +71,8 @@ card.forEach(function (i) {
         else if (target == `house-by-the-river-img`) {
             modalPhotoWindow.innerHTML = `<img
             style="
-            height:100%;
-            width: 100%;
-            overflow-x: hidden;
+            width: ${height}%;
+            margin: auto;
             "
             src="https://res2.weblium.site/res/5ce40621b84b1a002410eb9e/5ce420c7dc5e3800248c9cfe_optimized"
             alt="house by the river">`
