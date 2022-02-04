4759fba10cfa3419fe7e35e3bd50617d7ab02fcb
Fushtei Sviatoslav <fuschtei@gmail.com>
2022-02-04 09:29:06 +0200

    add new button for modal window

--- scripts/header.js
+++ scripts/header.js
@@ -1,3 +1,84 @@
+// import * as infoAboutUs from './bla.json'
+
+// console.log('infoAboutUs = '+infoAboutUs);
+
+
+// // let infoAboutUs = [{
+// //     "name": "Yosemite Mariposa ",
+// //     "place": "County, Ca, USA",
+// //     "numbers": [
+// //                 {"forText": "+38 (067) 123 45 67",
+// //                 "forCall": "+380671234567"},
+// //                 {"forText": "+38 (050) 123 45 67",
+// //                 "forCall": "+380501234567"}
+// //             ]
+// //     }];
+
+//     // let obj = JSON.parse(info.json);
+//     // console.log(obj);
+
+// // const response = await fetch ("info.json");
+// // const info = await response.json();
+// // console.log(info);
+
+// // fetch("info.json")
+// //   .then(response => response.json())
+// //   .then(json => console.log(json));
+
+
+// const header = document.querySelector("#header");
+// header.innerHTML += `
+// <div id="header-content-grid" class="container">
+//         <div class="header-content">
+//             <div class="logo-wrap" id="header-logo">
+//                 <a class="image-logo-a" href="index.html">
+//                     <img class="image-logo" src="images/logo.JPG" alt="logo-image">
+//                 </a>
+//             </div>
+//             <div id="three-line-icon" class="burger-icon threeLines">
+//                 <div class="burger-icon-line"></div>
+//             </div>
+//         </div>
+//         <div class="lock-display-980" id="nav-menu">
+//             <div class="nav-open" id="header-nav-open">
+//                 <div id="header-nav-links">
+//                     <a href="#" class="burger-link-font burger-link" id="header-about">About</a>
+//                     <a href="#" class="burger-link-font burger-link" id="header-galerry">Gallery</a>
+//                     <a href="#" class="burger-link-font burger-link" id="header-accommodation">Accommodation</a>
+//                     <a href="#" class="burger-link-font burger-link" id="header-how-to-get-there">How to get there</a>
+//                 </div>
+//                 <div class="places" id="header-places">
+//                     <div class="places-font">${infoAboutUs[0].name}</div>
+//                     <div class="places-font text-center">${infoAboutUs[0].place}</div>
+//                 </div>
+//                 <div class="numbers" id="header-numbers">
+//                 <a class="numbers-font" href="tel:${infoAboutUs[0].numbers[0].forCall}">${infoAboutUs[0].numbers[0].forText}</a>
+//                 <a class="numbers-font" href="tel:${infoAboutUs[0].numbers[1].forCall}">${infoAboutUs[0].numbers[1].forText}</a>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+//     <div id="clock"></div>`
+
+
+// const burger = document.querySelector('.burger-icon');
+// const burgerFunction = () => {
+//     document.body.classList.toggle("lock-page");
+
+//     const navOpenMenu = document.querySelector('#nav-menu');
+//     navOpenMenu.classList.toggle("lock-display-980");
+
+//     burger.classList.toggle("active");
+// };
+// burger.addEventListener("click", burgerFunction);
+
+
+
+
+
+
+
+
 let info = `[{
     "name": "Yosemite Mariposa ",
     "place": "County, Ca, USA",
--- scripts/images.js
+++ scripts/images.js
@@ -3,6 +3,7 @@ const modalPhotoWindow = document.querySelector(".modal-photo-window");
 const closeModalButton = document.querySelector(".close-modal-button");
 const zoomInModalButton = document.querySelector(".zoom-in-modal-button");
 const zoomOutModalButton = document.querySelector(".zoom-out-modal-button");
+const zoom0ModalButton = document.querySelector(".zoom-0-modal-button");
 const modalTopPanel = document.querySelector(".modal-top-panel");
 const darkBackground = document.querySelector(".dark-background");
 
@@ -17,90 +18,58 @@ card.forEach(function (i) {
 
         let target = event.target.id;
         let width = 100;
+        let interval;
         zoomInModalButton.addEventListener("click", () => {
             width += 50;
-            console.log(width)
-            return width;
-        })
+            if (width <= 400) {
+                return width;
+            }
+            else if (width > 400) {
+                width = 400;
+            }
+        });
+
         zoomOutModalButton.addEventListener("click", () => {
             width -= 50;
-            console.log(width)
-            return width;
-        })
+            if (width >= 100) {
+                return width;
+            }
+            else if (width < 100) {
+                width = 100;
+                return width;
+            }
+        });
 
-        if (target == `houses-on-hill-img`) {
-            function housesOnHillImg() {
-                modalPhotoWindow.innerHTML = `<img
-            class="big-modal-img"
-            style="
-            width:${width}%;
-            "
-            src="./images/images-for-zoom/houses-on-hill.jpeg"
-            alt="houses on hill">`}
-            setInterval(housesOnHillImg, 100)
+        zoom0ModalButton.addEventListener("click", () => {
+            width = 100;
+            return width
+        });
+
+        let changePhoto = (func) => {
+            interval = setInterval(func, 50)
+        };
+
+
+        function addImage() {
+            modalPhotoWindow.innerHTML = `<img
+        class="big-modal-img"
+        style="
+        width:${width}%;
+        "
+        src="./images/images-for-zoom/${target}.jpeg"
+        alt="${target}">`
         }
-        else if (target == `street-in-village-img`) {
-            function streetInVillageImg() {
-                modalPhotoWindow.innerHTML = `<img
-            class="big-modal-img"
-            style="
-            width:${width}%;
-            "
-            src="./images/images-for-zoom/street-in-village.jpeg"
-            alt="street in village">`}
-            setInterval(streetInVillageImg, 100)
-        }
-        else if (target == `big-house-img`) {
-            function bigHouseImg() {
-                modalPhotoWindow.innerHTML = `<img
-            class="big-modal-img"
-            style="
-            width:${width}%;
-            "
-            src="./images/images-for-zoom/big-house.jpeg"
-            alt="big house">`}
-            setInterval(bigHouseImg, 100)
-        }
-        else if (target == `lake-img`) {
-            function lakeImg() {
-                modalPhotoWindow.innerHTML = `<img
-            class="big-modal-img"
-            style="
-            width:${width}%;
-            "
-            src="./images/images-for-zoom/lake.jpeg"
-            alt="lake">`}
-            setInterval(lakeImg, 100)
-        }
-        else if (target == `mountains-img`) {
-            function mountainsImg() {
-                modalPhotoWindow.innerHTML = `<img
-            class="big-modal-img"
-            style="
-            width:${width}%;
-            "
-            src="./images/images-for-zoom/mountains.jpeg"
-            alt="mountains">`}
-            setInterval(mountainsImg, 100)
-        }
-        else if (target == `house-by-the-river-img`) {
-            function houseByTheRiverImg() {
-                modalPhotoWindow.innerHTML = `<img
-            class="big-modal-img"
-            style="
-            width:${width}%;
-            "
-            src="./images/images-for-zoom/house-by-the-river.jpeg"
-            alt="house by the river">`}
-            setInterval(houseByTheRiverImg, 100)
-        }
+        changePhoto(addImage);
+
+
+
+        closeModalButton.addEventListener("click", () => {
+            modalTopPanel.classList.add("display-none");
+            modalPhotoWindow.classList.add("display-none");
+            darkBackground.classList.add("display-none");
+            modalPhotoWindow.innerHTML = "";
+            clearInterval(interval);
+        });
     });
 });
 
-setInterval(500)
-
-closeModalButton.addEventListener("click", () => {
-    modalTopPanel.classList.add("display-none");
-    modalPhotoWindow.classList.add("display-none");
-    darkBackground.classList.add("display-none");
-});
