e03b75e15dd8b0cf73a675fcb9aa77e3a07b76e1
Fushtei Sviatoslav <fuschtei@gmail.com>
2022-02-19 16:43:50 +0200

    add scroll for section

--- scripts/scroll-div.js
+++ scripts/scroll-div.js
@@ -1,14 +1,14 @@
-// function onEntry(entry) {
-//     entry.forEach(change => {
-//         if (change.isIntersecting) {
-//             change.target.classList.add('active');
-//         }
-//     });
-// }
-// let options = { threshold: [0.5] };
-// let observer = new IntersectionObserver(onEntry, options);
-// let elements = document.querySelectorAll('.get-full-answer-for-your-question-content');
-// for (let elm of elements) {
-//     observer.observe(elm);
-// }
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('active');
+        }
+    });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.get-full-answer-for-your-question-content');
+for (let elm of elements) {
+    observer.observe(elm);
+}
 
